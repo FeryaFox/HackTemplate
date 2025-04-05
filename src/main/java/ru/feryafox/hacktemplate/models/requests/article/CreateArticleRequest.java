02b5f90f1bad993141e6d727d9b6a9a9e005d2cb
feryafox <bogdashkin145@gmail.com>
2025-04-05 11:42:19 +0300

    Удален Kotlin

---
+++ src/main/java/ru/feryafox/hacktemplate/models/requests/article/CreateArticleRequest.java
@@ -0,0 +1,16 @@
+package ru.feryafox.hacktemplate.models.requests.article;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateArticleRequest {
+
+    @NotNull
+    private String title;
+    private String content;
+}
