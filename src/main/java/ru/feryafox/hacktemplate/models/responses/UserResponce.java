7f8d2fc78cfa5a74c4d3cf7f42250b924d20aad5
Mark <markisme6383@gmail.com>
2025-04-05 11:16:21 +0300

    Добавил репозитории и дто для Task. начал писать сервис

--- src/main/java/ru/feryafox/hacktemplate/models/responses/UserResponce.java
+++ src/main/java/ru/feryafox/hacktemplate/models/responses/UserResponce.java
@@ -3,9 +3,12 @@ package ru.feryafox.hacktemplate.models.responses;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.feryafox.hacktemplate.entities.User;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Schema(description = "Пользователь, передаваемый в запросах")
 public class UserResponce {
 
@@ -13,5 +16,17 @@ public class UserResponce {
 
     private String middleName;
 
-    private String lastName;
+    private String surname;
+
+    public static UserResponce convertToUserResponse(User user) {
+        if (user == null) {
+            return null;
+        }
+
+        UserResponce response = new UserResponce();
+        response.setName(user.getFirstName());
+        response.setMiddleName(user.getMiddleName());
+        response.setSurname(user.getSurname());
+        return response;
+    }
 }
