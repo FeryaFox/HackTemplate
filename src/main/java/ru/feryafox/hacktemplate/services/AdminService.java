296884b6c1232ba0e5a7424ff0f8d4c0492791d6
feryafox <bogdashkin145@gmail.com>
2025-04-05 17:33:57 +0300

    Добавлен метод назначения админа

--- src/main/java/ru/feryafox/hacktemplate/services/AdminService.java
+++ src/main/java/ru/feryafox/hacktemplate/services/AdminService.java
@@ -3,10 +3,14 @@ package ru.feryafox.hacktemplate.services;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.feryafox.hacktemplate.entities.Role;
 import ru.feryafox.hacktemplate.models.responses.admin.GetAllUsersResponce;
+import ru.feryafox.hacktemplate.repositories.RoleRepository;
 import ru.feryafox.hacktemplate.repositories.UserRepository;
 
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -14,12 +18,23 @@ import java.util.stream.Collectors;
 @Transactional
 public class AdminService {
 
+    private final BaseService baseService;
+    private final RoleRepository roleRepository;
     UserRepository userRepository;
 
     public List<GetAllUsersResponce> getAllUsers() {
         return userRepository.findAll().stream()
                 .map(GetAllUsersResponce::fromUser)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void setUserAdmin(String userId) {
+        var user = baseService.getUserOrElseThrow(UUID.fromString(userId));
+
+        user.setRoles(Set.of(roleRepository.findByName(Role.RoleName.ROLE_ADMIN).get()));
+
+        userRepository.save(user);
     }
 
 }
